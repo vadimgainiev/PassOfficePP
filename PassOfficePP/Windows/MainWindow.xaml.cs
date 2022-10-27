ec33615957aced93ce615fbadc5fb6f03f1d7c36
Vadim Gainiev <vadim.gayniev@mail.ru>
2022-10-27 20:16:32 +0300

    Minor fixes.

--- PassOfficePP/Windows/MainWindow.xaml.cs
+++ PassOfficePP/Windows/MainWindow.xaml.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
 using PassOfficePP.Pages.DataPages;
 
 namespace PassOfficePP
